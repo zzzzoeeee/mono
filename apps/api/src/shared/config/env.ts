c9ca7a7cdaabc058de12d3dd95387f66bf32c7c3
Sutthinai <sutthinai.byyc@gmail.com>
2025-06-18 15:12:58 +0700

    refactor: change default SESSION_NAME to mono_session

--- apps/api/src/shared/config/env.ts
+++ apps/api/src/shared/config/env.ts
@@ -10,7 +10,7 @@ const envSchema = z.object({
 		z.array(z.string().url()).optional(),
 	),
 	SESSION_SECRET: z.string(),
-	SESSION_NAME: z.string().default('connect.sid'),
+	SESSION_NAME: z.string().default('mono_session'),
 });
 
 const env = envSchema.parse(process.env);
