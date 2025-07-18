7986d75bc24ac5b7f24ac2bf222f006152d24c0f
Sutthinai <sutthinai.byyc@gmail.com>
2025-07-18 19:42:46 +0700

    feat: Separate dockerfile for each app
    - Remove shared Dockerfile at workspace root.
    - Create dedicated Dockerfile for apps/api and apps/storefront

--- apps/storefront/next.config.ts
+++ apps/storefront/next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from "next";
+import path from 'node:path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	typescript: {
 		ignoreBuildErrors: true,
 	},
+	output: 'standalone',
+	outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 export default nextConfig;
